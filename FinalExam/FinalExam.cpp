1aed6147691b5944bc52087c0c3d6907eefae07a
christoforoschari <christoforos_chari@hotmail.com>
2022-05-06 10:00:53 +0300

    mm

--- FinalExam/FinalExam.cpp
+++ FinalExam/FinalExam.cpp
@@ -4,20 +4,29 @@
 *date 6/5/22
 */
 #include <iostream>
+#include<assert.h>
+#include<cmath>
+
 //global constatnt
 const int MAX_COLUMN = 3;
+
 using namespace std;
 
 //Fuction Prototypes.
 void enterData(int matrix[][MAX_COLUMN],int maxRow);
+void zeroOutData(int matrix[][MAX_COLUMN], int maxRow);
 
 
 /**
-
+*Present a menu to the users 
+*@return Return 0
 */
 int main()
 {
+    const int MAX_ROW = 2;
+    int matrix[MAX_ROW][MAX_COLUMN];
     int choice;
+    bool admin;
     do {
        
         cout << "\n1) Enter data in matrix";
@@ -29,9 +38,11 @@ int main()
 
         switch (choice) {
         case 1:
+            enterData(matrix, MAX_ROW);
             break;
 
         case 2:
+            zeroOutData(matrix, MAX_ROW);
             break;
 
         case 3:
@@ -48,7 +59,9 @@ int main()
 
 
         }
-    } while (4 != choice);
+    } while (4 != choice); {
+        cout << "Thankyou.";
+    }
 
 
 
@@ -58,6 +71,40 @@ int main()
 
 }
 
-void enterData(int matrix[][MAX_COLUMN], int maxRow)
+
+/**
+*Allows users to fill in the date for the matrix
+*@param matrix The matrix to be set to zeros
+*@param maxRow the number of rows in the matrix.
+*/
+void enterData(int matrix[][MAX_COLUMN],const int maxRow)
 {
+    assert(maxRow > 0);
+    for (int i = 0; i < maxRow; i++) {
+        for (int j = 0; j < MAX_COLUMN; j++) {
+            do {
+                cout << "\n Data for [" << i << "][" << j << "](-50 -- 50):";
+                cin >> matrix[i][j];
+            } while (matrix[i][j] <-50 || matrix[i][j]>50);
+            
+        }
+    }
+}
+
+/**
+*Data in matrix is filled with 0
+*@param matrix The matrix to be filled in
+*@param maxRow the number of rows in the matrix.
+*/
+
+void zeroOutData(int matrix[][MAX_COLUMN], int maxRow)
+{
+    assert(maxRow > 0);
+    for (int i = 0; i < maxRow; i++) {
+        for (int j = 0; j < MAX_COLUMN; j++) {
+            matrix[i][j] = 0;
+        
+        }
+    }
+
 }
